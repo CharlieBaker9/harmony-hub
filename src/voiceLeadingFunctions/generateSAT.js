4033fcee8c1bfec2977844a044650b8e89a10451
Charlie Baker <charles.d.baker.24@dartmouth.edu>
2024-04-10 23:58:58 -0400

    adding markers of decision points and what decision has to be made

--- src/voiceLeadingFunctions/generateSAT.js
+++ src/voiceLeadingFunctions/generateSAT.js
@@ -14,6 +14,7 @@ function assigningToRegister(s, a, t) {
   let soprano = s.map(function(number) { return number + 7;});
   let tenor = t.map(function(number) { return number - 7;});
   
+  //shifting alto
   let alto = [a[a.length-1]];
   for (let i = a.length - 2; i >= 0; i--){
     let temp = a[i];
@@ -39,35 +40,49 @@ function generateSAT(progression) {
   let s = [];
   let a = [];
   let t = [];
+  let d = [];
+  let decisionVoices = [];
 
   const progArr = progression.split(' ');
 
   let last = spacingDict[progArr[progArr.length-1]];
-
-  if (Array.isArray(last[0])) { 
+  let decision = false;
+  let dv = "";
+  if (Array.isArray(last[0])) {
+    dv = last;
     last = last[0];
+    decision = true;
   }
 
   s.push(last[0]);
   a.push(last[1]);
   t.push(last[2]);
+  d.push(decision);
+  decisionVoices.push(dv);
 
   for (let i = progArr.length - 1; i >= 1; i--){
+    let decisionPoint = false;
+    let decisionVoice = "";
     let key = progArr[i-1] + " " + progArr[i];
     let nextDict = progressionDict[key];
 
     if (Array.isArray(nextDict)){
       //decision point
+      decisionVoice = nextDict;
       nextDict = nextDict[0];
+      decisionPoint = true;
     }
 
     let idx = 0;
     addingNote(nextDict, s, idx);
     addingNote(nextDict, a, idx);
     addingNote(nextDict, t, idx);
+    d.unshift(decisionPoint);
+    decisionVoices.unshift(decisionVoice);
   }
-  
+
   let parts = assigningToRegister(s, a, t);
+  parts.push(d, decisionVoices);
 
   return parts;
 }
